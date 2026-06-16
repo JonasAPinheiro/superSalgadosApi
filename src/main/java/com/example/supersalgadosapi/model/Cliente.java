0f1bcc2ef54e9f901ed625fa31f54b31dc666a58
Jonas Pinheiro <jonas.pinheiro@aluno.ifsp.edu.br>
2026-06-15 23:38:41 -0300

    feat: Criação do projeto Spring

---
+++ src/main/java/com/example/supersalgadosapi/model/Cliente.java
@@ -0,0 +1,27 @@
+package com.example.supersalgadosapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private Double saldo = 0.0;
+}
---
+++ src/test/java/com/example/supersalgadosapi/SuperSalgadosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supersalgadosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperSalgadosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
